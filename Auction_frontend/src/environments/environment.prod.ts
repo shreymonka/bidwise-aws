1a7d7879fee97e15c89ab9fd0c5fee05f63ce005
shreymonka <58852819+shreymonka@users.noreply.github.com>
2024-12-03 18:23:41 -0400

    Taking credentials as environment variables

--- Auction_frontend/src/environments/environment.prod.ts
+++ Auction_frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://3.208.93.111:8080/api/v1'
+    apiUrl: '${BACKEND_API_URL}'
   };
--- Auction_frontend/src/environments/environment.ts
+++ Auction_frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://3.208.93.111:8080/api/v1'
+  apiUrl: '${BACKEND_API_URL}'
 };
